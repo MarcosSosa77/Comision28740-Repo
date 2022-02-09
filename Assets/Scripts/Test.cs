570a1ad7e92f0da09fa0d45df8b275861881b915
MarcosSosa77 <marquitos.sosa69@gmail.com>
2022-02-08 21:53:12 -0300

    Proto-SistemaRespawnActualizado
    Aca adrian trabajo con ladrillos y los ++

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,6 +5,7 @@ public class Test : MonoBehaviour
 
     int monedas = 1;//variable creada para la recolecciòn
     
+    int ladrillos = 0;
     void Start()
     {
        //Aqui deberia inicializar los stats del jugador
@@ -15,12 +16,19 @@ public class Test : MonoBehaviour
         GanarMonedas();
         //MovimientoJugaod();
         //DisparoArma();
+        RecolectarLadrillos();
     }   
 
     void GanarMonedas()
     {
         monedas++;
         Debug.Log("Recolecte: "+monedas+" monedas");
+    }
+
+    void RecolectarLadrillos()
+    {
+        ladrillos++;
+        Debug.Log("Adrian esta recolectando :" + ladrillos+" ladrillos");
     }
 
 }
