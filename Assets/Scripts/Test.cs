81add4fe1a429afb4afbeb632fe48a0ef7241d15
MarcosSosa77 <marquitos.sosa69@gmail.com>
2022-02-08 21:15:47 -0300

    V- 0.0.1
    Commit Proyecto Unity  nuevo

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    int monedas = 1;
+    
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {      
+        GanarMonedas();
+    }   
+
+    void GanarMonedas()
+    {
+        monedas++;
+        Debug.Log("Recolecte: "+monedas+" monedas");
+    }
+
+}
